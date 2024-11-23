2116e38688cd3d9c5d297aee80a550bdef628bad
yammyso <yammyso02@outlook.com>
2024-11-23 15:41:37 -0500

    Create ChildAnimal.py

---
+++ ChildAnimal.py
@@ -0,0 +1,7 @@
+from main_repository.animal import Animal
+class Lion(Animal):
+    def __init__(self, name, breed):
+        super().__init__(name)
+        self.breed = breed    
+    def speak(self):
+        return f"{self.name} scream ROAR!"
