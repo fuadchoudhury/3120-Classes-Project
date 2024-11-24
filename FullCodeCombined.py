6eefa11d121b94f5c722a32d5f72383ffb67adf3
Fuad Choudhury <fuad.choudhury1@baruchmail.cuny.edu>
2024-11-23 22:42:51 -0500

    FullCombinedCode.py
    Here is full combined code including parent & child class.

---
+++ FullCodeCombined.py
@@ -0,0 +1,30 @@
+from abc import ABC, abstractmethod
+class Animal(ABC):
+    def __init__(self, name):
+        self.name = name
+    
+    def get_name(self):
+        return self.name
+    @abstractmethod
+    def make_sound(self):
+        pass
+
+class Lion(Animal):
+    def __init__(self, name, breed):
+        super().__init__(name)
+        self.breed = breed
+    def make_sound(self):
+        return "ROAR!"
+    def describe(self):
+        return f"{self.name} is {self.breed} breed of lion and says {self.make_sound()}."
+
+lions = [
+    Lion(name="Mufasa", breed="Asiatic Lion"),
+    Lion(name="Scar", breed="Barbary Lion"),
+    Lion(name="Simba", breed="African Lion"),
+    Lion(name="Nala", breed="Cape Lion"),
+]
+
+if __name__ == "__main__":
+    for lion in lions:
+        print(lion.describe())
